dd376a4052047ba9d5712c6ff7ef2673aef1d9c7
zulfiqarhussainrizvi <zulfikar.hussain.rizvi@gmail.com>
2025-08-08 13:25:22 +0530

    testing

--- order-service/src/main/java/com/order/entity/Order.java
+++ order-service/src/main/java/com/order/entity/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "orders")
 public class Order {
 
     @Id
